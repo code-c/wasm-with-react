488232f1cb2101def8a308b7d754a86eb32ea3f7
code-c <ccottrel@uvm.edu>
2021-03-16 23:20:40 -0400

    got image to appear on screen and attemped to parse string and push more images on, saving now

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import DrawMap from './DrawMap';
 
 const App = () => (
     <div className="container">
         <h1>Hello World, React!</h1>
+        <DrawMap/>
     </div>
 )
 
--- src/DrawMap.jsx
+++ src/DrawMap.jsx
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-export default class Header extends Component {
-  render() {
-    return (
-      <div className="row">
-        <div className="logo">
-          <img src={logo} width="100" height="50" />
+import borderShadow from './images/shadow.png';
+import grass from './images/tiles/grass0.png';
+import water from './images/tiles/water0.png';
+import bridgeStart from './images/tiles/bridge0_start.png';
+import bridgeEnd from './images/tiles/bridge0_end.png';
+import bridgeMid from './images/tiles/bridge0_mid.png';
+
+
+
+const mapString =`
+# # # # # # # # # # # # # # # # # # # #
+# · · · · · · · · · · · · · · · · · · #
+# · · · · · · · · · · · · · · · · · · #
+# · · · · · · · · · · · · · · · · · · #
+# · · · · · · · · · · · · · · · · · · #
+# W W W W W W W W W BE W W W W W W W W # 
+# W W W W W W W W W W W W W W W W W W # 
+# W W W W W W W W W BS W W W W W W W W # 
+# · · · · · · · · · · · · · · · · · · #
+# · · · · · · · · · · · · · · · · · · #
+# · · · · · · · · · · · · · · · · · · #
+# · · · · · · · · · · · · · · · · · · #
+# # # # # # # # # # # # # # # # # # # #
+`
+
+export default class DrawMap extends Component {
+
+    Map(mapString) {
+        for (tileChar in mapString) {
+            switch(tileChar){
+                case '#':
+                    return <img src={borderShadow}></img>;
+                case '·':
+                    return <img src={grass}></img>;
+                case 'w':
+                    return <img src={water}></img>;
+                case 'BS':
+                    return <img src={bridgeStart}></img>;
+                case 'BE':
+                    return <img src={bridgeEnd}></img>;
+                case 'BM':
+                    return <img src={bridgeMid}></img>;
+                default:
+                    return null;           
+            }
+        }
+    }
+
+    myImage() {
+        return (
+        <div className="tile">
+            <img src={grass}></img>
         </div>
-      </div>
-    );
-  }
+        );
+    }
+
+    render() {
+        return (
+            <div classMap>
+                <img src={grass}></img>
+                </div>
+        );
+    }
 } 
