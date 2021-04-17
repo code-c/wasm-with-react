b2cc070faf0874aa42ef0438c1fc9b7053e1c338
code-c <ccottrel@uvm.edu>
2021-04-16 22:41:43 -0400

    animated character movement

--- src/game/Character.js
+++ src/game/Character.js
@@ -1,20 +1,220 @@
 import * as PIXI from "pixi.js";
 
+let playerSheet = {};
+let speed = 2;
+let direction = "";
+let carrying = false;
 
 export default class Character extends PIXI.AnimatedSprite {
-    constructor(x = 0, y = 0, texture, name = "phil", hp="100") {
+    constructor(x = 0, y = 0, name = "phil", hp="100") {
         // like PIXI.texture.from to create sprite from texture
         //fromFrames(textures);
-        super(texture);
-        console.log('character texture', this.textures)
+        createPlayerSheet();
+        super(playerSheet.standSouth);
         //work with it set values
-        //this.animationSpeed = .5;
+        this.animationSpeed = .2;
         this.anchor.set(0.5);
-        //this.loop = true;
+        this.loop = false;
         this.name = name;
 
         this.hp=hp;
         this.y = y;
         this.x = x;
     }
+
+    moveNorth() {
+        direction = "north";
+        if(!this.playing){
+            this.textures = playerSheet.walkNorth;
+            this.play();
+        }
+        this.y -= speed;
+    }
+
+    moveSouth() {
+        direction = "south";
+        if(!this.playing){
+            this.textures = playerSheet.walkSouth;
+            this.play();
+        }
+        this.y += speed;
+    }
+
+    moveEast() {
+        direction = "east";
+        if(!this.playing){
+            this.textures = playerSheet.walkEast;
+            this.play();
+        }
+        this.x += speed;
+    }
+
+    moveWest() {
+        direction = "west";
+        if(!this.playing){
+            this.textures = playerSheet.walkWest;
+            this.play();
+        }
+        this.x -= speed;
+    }
+
+    pickup() {
+        if(!this.playing){
+            switch(direction){
+                case "north":
+                    this.textures = playerSheet.pickupNorth;
+                    this.textures.reverse;
+                    this.play();
+                    break;
+                case "south":
+                    this.textures = playerSheet.pickupSouth;
+                    this.play();
+                    break;
+                case "east":
+                    this.textures = playerSheet.pickupEast;
+                    this.play();
+                    break;
+                case "west":
+                    this.textures = playerSheet.pickupWest;
+                    this.play();
+                    break;
+                default:
+                    this.textures = playerSheet.pickupSouth;
+
+            }
+        }
+        carrying = true;
+    }
+
+    place() {
+        if(!this.playing){
+            switch(direction){
+                case "north":
+                    this.textures = playerSheet.pickupNorth;
+                    this.play();
+                    break;
+                case "south":
+                    this.textures = playerSheet.pickupSouth;
+                    this.play();
+                    break;
+                case "east":
+                    this.textures = playerSheet.pickupEast;
+                    this.play();
+                    break;
+                case "west":
+                    this.textures = playerSheet.pickupWest;
+                    this.play();
+                    break;
+                default:
+                    this.textures = playerSheet.pickupSouth;
+
+            }
+        }
+        carrying = true;
+    }
+}
+
+function createPlayerSheet() {
+    const loader = new PIXI.Loader();
+        
+    // add resources to the loader to use later
+    loader.add('character', './src/game/images/character.json'); //character
+
+    // load standing directions from stylesheet
+    playerSheet["standNorth"] = [
+        new PIXI.Texture.from('player-north-0.png')
+    ];
+    playerSheet["standSouth"] = [
+        new PIXI.Texture.from('player-south-0.png')
+    ];
+    playerSheet["standEast"] = [
+        new PIXI.Texture.from('player-east-0.png')
+    ];
+    playerSheet["standWest"] = [
+        new PIXI.Texture.from('player-west-0.png')
+    ];
+
+    //load walking directions from stylesheet
+    playerSheet["walkNorth"] = [
+        new PIXI.Texture.from('player-north-0.png'),
+        new PIXI.Texture.from('player-north-1.png'),
+        new PIXI.Texture.from('player-north-2.png'),
+        new PIXI.Texture.from('player-north-3.png'),
+        new PIXI.Texture.from('player-north-0.png')
+    ];
+    playerSheet["walkSouth"] = [
+        new PIXI.Texture.from('player-south-0.png'),
+        new PIXI.Texture.from('player-south-1.png'),
+        new PIXI.Texture.from('player-south-2.png'),
+        new PIXI.Texture.from('player-south-3.png'),
+        new PIXI.Texture.from('player-south-0.png')
+    ];
+    playerSheet["walkEast"] = [
+        new PIXI.Texture.from('player-east-0.png'),
+        new PIXI.Texture.from('player-east-1.png'),
+        new PIXI.Texture.from('player-east-2.png'),
+        new PIXI.Texture.from('player-east-3.png'),
+        new PIXI.Texture.from('player-east-0.png')
+    ];
+    playerSheet["walkWest"] = [
+        new PIXI.Texture.from('player-west-0.png'),
+        new PIXI.Texture.from('player-west-1.png'),
+        new PIXI.Texture.from('player-west-2.png'),
+        new PIXI.Texture.from('player-west-3.png'),
+        new PIXI.Texture.from('player-west-0.png')
+    ];
+
+    //load pickup directions from stylesheet
+    playerSheet["pickupNorth"] = [
+        new PIXI.Texture.from('player-pickup-north-0.png'),
+        new PIXI.Texture.from('player-pickup-north-1.png'),
+        new PIXI.Texture.from('player-pickup-north-2.png')
+    ];
+    playerSheet["pickupSouth"] = [
+        new PIXI.Texture.from('player-pickup-south-0.png'),
+        new PIXI.Texture.from('player-pickup-south-1.png'),
+        new PIXI.Texture.from('player-pickup-south-2.png')
+    ];
+    playerSheet["pickupEast"] = [
+        new PIXI.Texture.from('player-pickup-east-0.png'),
+        new PIXI.Texture.from('player-pickup-east-1.png'),
+        new PIXI.Texture.from('player-pickup-east-2.png')
+    ];
+    playerSheet["pickupWest"] = [
+        new PIXI.Texture.from('player-pickup-west-0.png'),
+        new PIXI.Texture.from('player-pickup-west-1.png'),
+        new PIXI.Texture.from('player-pickup-west-2.png')
+    ];
+
+    //carrying directions from stylesheet
+    playerSheet["carryNorth"] = [
+        new PIXI.Texture.from('player-carry-north-0.png'),
+        new PIXI.Texture.from('player-carry-north-1.png'),
+        new PIXI.Texture.from('player-carry-north-2.png'),
+        new PIXI.Texture.from('player-carry-north-3.png')
+    ];
+    playerSheet["carrySouth"] = [
+        new PIXI.Texture.from('player-carry-south-0.png'),
+        new PIXI.Texture.from('player-carry-south-1.png'),
+        new PIXI.Texture.from('player-carry-south-2.png'),
+        new PIXI.Texture.from('player-carry-south-3.png')
+    ];
+    playerSheet["carryEast"] = [
+        new PIXI.Texture.from('player-carry-east-0.png'),
+        new PIXI.Texture.from('player-carry-east-1.png'),
+        new PIXI.Texture.from('player-carry-east-2.png'),
+        new PIXI.Texture.from('player-carry-east-3.png')
+    ];
+    playerSheet["carryWest"] = [
+        new PIXI.Texture.from('player-carry-west-0.png'),
+        new PIXI.Texture.from('player-carry-west-1.png'),
+        new PIXI.Texture.from('player-carry-west-2.png'),
+        new PIXI.Texture.from('player-carry-west-3.png')
+    ];
+
+    playerSheet["acquire"] = [
+        new PIXI.Texture.from("player-acquire-0.png")
+    ];
+    
+    console.log(playerSheet);
 }
--- src/game/Map.js
+++ src/game/Map.js
@@ -16,12 +16,11 @@ export default class Map extends React.Component {
 
     //using componenet did mount for PIXI
     componentDidMount() {
-        
         // PIXI renderer and settings
         const renderer = PIXI.autoDetectRenderer({
             antialias: true,
             autoDensity: true,
-            resolution: window.devicePixelRatio || 2,
+            resolution: window.devicePixelRatio || 1,
             width: 800,
             height: 600,
         });
@@ -30,14 +29,20 @@ export default class Map extends React.Component {
         let stage;
         let tilemap;
         let frame = 0;
+        let keys = {};
 
         //characters in this map
         let player;
-        let playerSheet;
+        let player2;
+        let playerSheet = {};
         let stumpy;
         
         // append to the body
         document.body.appendChild(renderer.view);
+
+        // listen for keys pressed
+        window.addEventListener("keydown", keysDown);
+        window.addEventListener("keyup", keysUp);
         
         //create the PIXI loader
         const loader = new PIXI.Loader();
@@ -55,51 +60,66 @@ export default class Map extends React.Component {
             stage.addChild(tilemap);
 
             // create player
-            createPlayerSheet();
             loadPlayer();
-            // // createCharacter();
-            // // console.log(player)
-            //stage.addChild(player);
         
             // Setup rendering loop
             PIXI.Ticker.shared.add(() => renderer.render(stage));
+            PIXI.Ticker.shared.add(gameLoop);
 
         
             makeTilemap();
             //createCharacter();
         });
 
-        function createPlayerSheet() {
-            let styleSheet = loader.resources['character'].textures;
+        function keysDown(keyEvent) {
+            console.log(keyEvent.keyCode);
+            keys[keyEvent.keyCode] = true;
+        }
 
-            // playerSheet["standSouth"] = [
-            //     new PIXI.Texture(styleSheet['player-south-0'])
-            // ];
-            // playerSheet["standWest"] = [
-            //     new PIXI.Texture(styleSheet['player-west-0'])
-            // ];
-            // playerSheet["standEast"] = [
-            //     new PIXI.Texture(styleSheet['player-east-0'])
-            // ];
-            // playerSheet["standNorth"] = [
-            //     new PIXI.Texture(styleSheet['player-north-0'])
-            // ];
+        function keysUp(keyEvent) {
+            keys[keyEvent.keyCode] = false;
+        }
 
-            playerSheet = styleSheet;
+        function gameLoop() {
             
-            console.log(typeof(playerSheet.standSouth));
+            // W (north)
+            if(keys["87"]) {
+                player2.moveNorth();
+            }
+
+            // A (west)
+            if(keys["65"]) {
+                player2.moveWest();
+            }
+
+            // S (south)
+            if(keys["83"]) {
+                player2.moveSouth();
+            }
+
+            // D (east)
+            if(keys["68"]) {
+                player2.moveEast();
+            }
+
+            // shift (pickUp)
+            if (keys["16"]) {
+                player2.pickup();
+            }
         }
 
         function loadPlayer() {
-            let styleSheet = loader.resources['character'].textures;
-            player = new PIXI.AnimatedSprite(styleSheet.animations["player-west-0"]);
-            player.anchor.set(0.5);
-            player.animationSpeed = .5;
-            player.loop = false;
-            player.x = renderer.view.width/2;
-            player.y = renderer.view.height/2;
-            stage.addChild(player);
-            //player.play();
+            const pxW = renderer.screen.width;
+            const pxH = renderer.screen.height;
+            player2 = new Character(
+                pxW/2,
+                pxH/2,
+                "mainDude",
+                "100"
+            );
+            console.log("playerSheet: ", player2);
+            stage.addChild(player2);
+            //player2.play();
         }
         
         function makeTilemap() {
@@ -126,27 +146,10 @@ export default class Map extends React.Component {
                     );
                 }
             }
-        
             // Button does not appear in the atlas, but @pixi/tilemap won't surrender
             // - it will create second layer for special for buttons and they will
             // appear above all the other tiles.
-            tilemap.tile('player-south-0', 0, 0);
-        }
-
-        // create character function
-        function createCharacter() {
-            const resources = loader.resources;
-            let charSheet = resources['character'].textures;
-            console.log('player textures', resources['character'].textures);
-            console.log('south', charSheet['player-south-0'])
-            player = new Character(
-                renderer.screen.width/2, 
-                renderer.screen.height/2, 
-                charSheet, 
-                "Player", 
-                100
-            )
-            //player.play()
+            //tilemap.tile('player-south-0', 0, 0);
         }
     
     };
